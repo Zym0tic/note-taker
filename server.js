e9b3a84a23a3d4c0d6e3493184d7142896241109
zym0tic <lukerozon@gmail.com>
2022-08-18 18:49:01 -0400

    edited file structure. added get requests inserver

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+  });
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(`${req.method} request received to get notes`);
-    console.info(`${req.method} request received to get notes`);
+  app.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    console.log(data);
   });
-  
+
+  app.get("/api/notes", (req, res) => {
+    fs.readFile(".db/db.json", "utf-8", (err, data) => {
+      if(err) {
+
+      } else {
+
+      } 
+    })
+  })
 
 
 
