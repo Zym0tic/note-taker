09669a76624ee3de0f101ae2fe39e4f6b1436bb4
zym0tic <lukerozon@gmail.com>
2022-08-18 21:14:31 -0400

    saving

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const uuid = require("./helpers/uuid");
-const fsUtils = require("./helpers/fsUtils");
+// const fsUtils = require("./helpers/fsUtils");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
